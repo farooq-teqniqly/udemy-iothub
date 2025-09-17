35c6b8f77dd0e58a8a5dd8f73c34184ca587de64
farooq-teqniqly <farooq@teqniqly.com>
2025-09-17 16:50:34 -0700

    chore: Add device twin sample code demonstrating tag and desired property updates.

---
+++ DeviceTwinSampleProject/DeviceTwinBackend/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices;
+
+namespace DeviceTwinBackend
+{
+    class Program
+    {
+        static RegistryManager registryManager;
+
+        // CHANGE THE CONNECTION STRING TO THE ACTUAL CONNETION STRING OF THE IOT HUB (SERVICE POLICY)
+        static string connectionString = "";
+
+        public static async Task SetDeviceTags(string deviceId)
+        {
+            var twin = await registryManager.GetTwinAsync(deviceId);
+
+            var patch =
+                @"{
+                        tags: {
+                            location:  {
+                                country: 'USA',
+                                city: 'Seattle'
+                            }
+                        },
+                        properties: {
+                            desired:  {
+                                FPS: 30
+                            }
+                        }
+                    }";
+
+            await registryManager.UpdateTwinAsync(twin.DeviceId, patch, twin.ETag);
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Starting Device Twin backend...");
+            registryManager = RegistryManager.CreateFromConnectionString(connectionString);
+            SetDeviceTags("iot-dev-01").Wait();
+            Console.WriteLine("Hit Enter to exit...");
+            Console.ReadLine();
+        }
+    }
+}
