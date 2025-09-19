7cd0da2e5f8fb52a76ff115e8d56ef9133df8c47
farooq-teqniqly <farooq@teqniqly.com>
2025-09-19 13:22:20 -0700

    chore: Invoking direct methods example.

---
+++ DeviceMethod/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+
+namespace AzCourse.IoT.Method
+{
+    public class Program
+    {
+        const string DEVICE_CONNECTION_STRING =
+            "HostName=udemy-iot-course-hub.azure-devices.net;DeviceId=iot-dev-dps-01;SharedAccessKey=xxxx";
+
+        public static async Task<int> Main(string[] args)
+        {
+            using var deviceClient = DeviceClient.CreateFromConnectionString(
+                DEVICE_CONNECTION_STRING,
+                TransportType.Amqp
+            );
+            var sample = new MethodSample(deviceClient);
+            await sample.RunMethod();
+            await deviceClient.CloseAsync();
+
+            Console.WriteLine("Done.");
+            return 0;
+        }
+    }
+}
