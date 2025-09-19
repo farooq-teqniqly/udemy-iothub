a3f9445987ad7e04187859343906b12494ad694a
farooq-teqniqly <farooq@teqniqly.com>
2025-09-19 13:38:10 -0700

    chore: Jobs example.

--- InvokeDeviceMethod/Program.cs
+++ InvokeDeviceMethod/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.Azure.Devices;
 
@@ -6,10 +7,11 @@ namespace InvokeDeviceMethod
 {
     internal class Program
     {
+        private static JobClient jobClient;
         private static ServiceClient serviceClient;
 
         private static string connectionString =
-            "HostName=udemy-iot-course-hub.azure-devices.net;SharedAccessKeyName=service;SharedAccessKey=xxxx";
+            "HostName=udemy-iot-course-hub.azure-devices.net;SharedAccessKeyName=iothubowner;SharedAccessKey=xxxxx";
         private static string deviceId = "iot-dev-dps-01";
 
         private static async Task Main(string[] args)
@@ -18,7 +20,8 @@ namespace InvokeDeviceMethod
 
             serviceClient = ServiceClient.CreateFromConnectionString(connectionString);
 
-            await InvokeDirectMethodAsync();
+            //await InvokeDirectMethodAsync();
+            await ScheduleMethodJob();
 
             serviceClient.Dispose();
 
@@ -44,6 +47,46 @@ namespace InvokeDeviceMethod
             Console.WriteLine(
                 $"\nResponse status: {response.Status}, payload:\n\t{response.GetPayloadAsJson()}"
             );
+        }
+
+        private static async Task ScheduleMethodJob()
+        {
+            jobClient = JobClient.CreateFromConnectionString(connectionString);
+
+            string methodJobId = Guid.NewGuid().ToString();
+
+            var methodName = "SetFlashlightState";
+
+            var methodInvocation = new CloudToDeviceMethod(methodName)
+            {
+                ResponseTimeout = TimeSpan.FromSeconds(30),
+            };
+            methodInvocation.SetPayloadJson("\"On\"");
+
+            JobResponse result = await jobClient.ScheduleDeviceMethodAsync(
+                methodJobId,
+                $"DeviceId IN ['{deviceId}']",
+                methodInvocation,
+                DateTime.UtcNow.AddSeconds(20),
+                (long)TimeSpan.FromMinutes(2).TotalSeconds
+            );
+
+            Console.WriteLine("Started Method Job");
+
+            MonitorJob(methodJobId).Wait();
+            Console.WriteLine("Press ENTER to run the next job.");
+            Console.ReadLine();
+        }
+
+        public static async Task MonitorJob(string jobId)
+        {
+            JobResponse result;
+            do
+            {
+                result = await jobClient.GetJobAsync(jobId);
+                Console.WriteLine("Job Status : " + result.Status.ToString());
+                Thread.Sleep(2000);
+            } while ((result.Status != JobStatus.Completed) && (result.Status != JobStatus.Failed));
         }
     }
 }
